b3877f74eca881732449b4b2d8ea74e5b54f185c
Kashish Agrawal <81006282+Kashish0401@users.noreply.github.com>
2022-07-29 01:22:23 +0530

    Detect cycle in a linked list

--- src/LeetCode_Solutions/LinkedListCycle.java
+++ src/LeetCode_Solutions/LinkedListCycle.java
@@ -1,107 +1,33 @@
-class MyLinkedList {
-    class Node{
-        int element;
-        Node next;
-        public Node(int e, Node n){
-            element=e;
-            next=n;
-        }
-    }
-    
-    private Node head;
-    private Node tail;
-    private int size;
-
-    public MyLinkedList() {
-        head=tail=null;
-        size=0;
-    }
-    
-    public int get(int index) {
-        if(index<0 || index>=size)
-            return -1;
-        Node p=head;
-        for(int i=0;i<=index-1;i++){
-            p=p.next;
-        }
-        return p.element;
-    }
-    
-    public void addAtHead(int val) {
-        Node newest= new Node(val,null);
-        if(size==0){
-            tail=newest;
-        }
-        else{
-            newest.next=head;
-        }
-        head=newest;
-        size++;
-    }
-    
-    public void addAtTail(int val) {
-        Node newest= new Node(val,null);
-        if(size==0){
-            head=newest;
-        }
-        else{
-            tail.next=newest;
-        }
-        tail=newest;
-        size++;
-    }
-    
-    public void addAtIndex(int index, int val) {
-        if(index<0 || index>size)
-            return;
-        if(index==0){
-            addAtHead(val);
-        }
-        else if(index==size){
-            addAtTail(val);
-        }
-        else{
-            Node newest= new Node(val,null);
-            Node p=head;
-            for(int i=0;i<index-1;i++){
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null)
+            return false;
+        ListNode p=head,q=head;
+        if(p.next==null)
+            return false;
+        if(p.next==q.next.next)
+            return true;
+        while(p.next!=null){
+            if(q.next==null || q.next.next==null)
+                return false;
+            else{
+                q=q.next.next;
                 p=p.next;
             }
-            newest.next=p.next;
-            p.next=newest;
-            size++;
+            if(p.next==q.next)
+                return true;
         }
-    }
-    
-    public void deleteAtIndex(int index) {
-        if(index<0 || index>=size)
-            return;
-        if(size==1){
-            head=tail=null;
-            size--;
-        }
-        else if(index==0){
-            head=head.next;
-            size--;
-        }
-        else{
-            Node p=head;
-            for(int i=0;i<index-1;i++){
-                p=p.next;
-            }
-            p.next=p.next.next;
-            size--;
-            if(size==index)
-                tail=p;
-        }
+        return false;
     }
 }
-
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
- */
