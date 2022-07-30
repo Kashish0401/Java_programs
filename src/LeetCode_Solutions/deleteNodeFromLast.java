e3280512bf6439e3f5227c201a3d9971df1aa734
Kashish Agrawal <81006282+Kashish0401@users.noreply.github.com>
2022-07-30 07:23:14 +0530

    deletes node from last at given pos

---
+++ src/LeetCode_Solutions/deleteNodeFromLast.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int size=1;
+        ListNode p=head;
+        while(p.next!=null){
+            size++;
+            p=p.next;
+        }
+        p=head;
+        for(int i=0;i<(size-n-1);i++){
+            p=p.next;
+        }
+        if(size==1)
+            return null;
+        if(n==size)
+            head=head.next;
+        if(n==1){
+            p.next=null;
+        }
+        else
+            p.next=p.next.next;
+        return head;
+    }
+}
