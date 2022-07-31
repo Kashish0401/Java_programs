8ca39d2807bf865d3583501d02ca1fa8d2c91c56
Kashish Agrawal <81006282+Kashish0401@users.noreply.github.com>
2022-07-31 17:53:02 +0530

    It deletes desires elements of a linked list

---
+++ src/LeetCode_Solutions/deleteDesiredElements.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode p=head;
+        while(p!=null){
+            if(p.next!=null && p.next.val==val){
+                    p.next=p.next.next;
+            }
+           else
+               p=p.next;
+        }
+        if(head!=null && head.val==val)
+            head=head.next;
+        return head;
+    }
+}
